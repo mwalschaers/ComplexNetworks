f3859b51e40f19385cc2d13c799034201f72ae21
mwalschaers <87516792+mwalschaers@users.noreply.github.com>
2021-07-16 09:53:46 +0200

    Update BA-Graph-State.sh

--- BA-Graph-State.sh
+++ BA-Graph-State.sh
@@ -11,12 +11,9 @@ squeezing=3
 repetitions=1
 random=0
 id=1234
-pathway=/Users/mattiauser/ownCloud/ComplexNetworks/BA_n${nodes}_m${parameter}_sq${squeezing}_randomSub${random}_ID${id}
-#pathway=/users/jussieu/walschaers/Graph-States/BA_n${nodes}_m${parameter}_sq${squeezing}_randomSub${random}_ID$SLURM_JOBID
-
-#source activate mattia_graph_env
+pathway=[INSERT LOCAL DIRECTORY PATH]BA_n${nodes}_m${parameter}_sq${squeezing}_randomSub${random}_ID${id}
 
 mkdir -p $pathway
 cd $pathway
 
-python3 /Users/mattiauser/ownCloud/ComplexNetworks/IntensityCorrBA_Cluster_Stat.py $id $nodes $parameter $squeezing $repetitions $random
+python3 [INSERT LOCAL DIRECTORY PATH]IntensityCorrBA_Cluster_Stat.py $id $nodes $parameter $squeezing $repetitions $random
